fa4a852cbf10b33ccd3a0595d36db50f9a2fea31
elvis kahoro <elvis@reflex.dev>
2024-10-25 09:39:24 -0700

    chroma

---
+++ hackathon_project_tracker/helper_chroma/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["set_up_client_from_tokens"]
+
+from .helper_chroma import set_up_client_from_tokens
--- hackathon_project_tracker/tokens.py
+++ hackathon_project_tracker/tokens.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 import os
 
 TOKENS: dict[str, str] = {
+    "CHROMA_DATABASE": "devxhackathonprojecttracker",
+    "CHROMA_API_KEY": os.getenv("CHROMA_API_KEY"),
+    "CHROMA_TENANT": os.getenv("CHROMA_TENANT"),
     "GITHUB_OWNER": "reflex-dev",
     "GITHUB_REPO": "reflex",
     "GITHUB_CLIENT_ID": os.getenv("GITHUB_CLIENT_ID"),
