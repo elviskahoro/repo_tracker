3144f49031c7b387d2f3e0dd5a9ea6040e87131d
elvis kahoro <elvis@reflex.dev>
2024-10-31 14:41:08 -0700

    temp

--- rxconfig.py
+++ rxconfig.py
@@ -2,4 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="hackathon_project_tracker",
+    # db_url="postgresql://devx_owner:W0iQkwxAMSc9@ep-damp-block-a6xeq9w6.us-west-2.aws.neon.tech/devx?sslmode=require&options=endpoint%3Dep-damp-block-a6xeq9w6",
 )
