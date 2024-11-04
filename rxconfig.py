9f0e141f938f101a92733049a0aa3685adb3246e
elvis kahoro <elvis@reflex.dev>
2024-11-04 11:08:39 -0800

    readding

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="hackathon",
-    # db_url="postgresql://devx_owner:W0iQkwxAMSc9@ep-damp-block-a6xeq9w6.us-west-2.aws.neon.tech/devx?sslmode=require&options=endpoint%3Dep-damp-block-a6xeq9w6",
+    db_url="postgresql://devx_owner:W0iQkwxAMSc9@ep-damp-block-a6xeq9w6.us-west-2.aws.neon.tech/devx?sslmode=require&options=endpoint%3Dep-damp-block-a6xeq9w6",
 )
